f13baa8a997ba5fca5f4d3dd1b969f46803f43df
Madalin Bogdea <madalinbogdea@icloud.com>
2025-09-13 20:23:54 +0300

    chore: setup firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpdJo7H-0R-2ozGcZ0Up9Q4zBSAjVkLUg",
+  authDomain: "notes-app-d4d33.firebaseapp.com",
+  projectId: "notes-app-d4d33",
+  storageBucket: "notes-app-d4d33.firebasestorage.app",
+  messagingSenderId: "479595742105",
+  appId: "1:479595742105:web:114999dcda4ec468554908",
+};
+
+const app = initializeApp(firebaseConfig);
